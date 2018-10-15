05ee938fafa2a40a4e502407bd032abc0138c9d1
Svyatoslav-Polishchuk <hello@karabeen.com>
2018-10-15 22:14:21 +0300

    minor update

---
+++ a/js/main.js
@@ -0,0 +1,25 @@
+$(".link-nav").on({
+  mouseenter: function () {
+    $(this).addClass("this-hovered");
+    $(".link-nav").addClass
+  },
+  mouseleave: function () {
+    $(this).removeClass("this-hovered");
+  }
+});
+
+$(".link-contacts").on({
+  mouseenter: function () {
+    $(this).addClass("this-hovered");
+    $(".link-contacts").addClass
+  },
+  mouseleave: function () {
+    $(this).removeClass("this-hovered");
+  }
+});
+
+
+$("#getConacts").click(function() {
+  $(".header").toggleClass("contacts-shown");
+  $(".shading-container").toggleClass("shown");
+})
