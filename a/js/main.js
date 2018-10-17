b7bed5458d173b3cc4784e57a9cc873314d93b61
Svyatoslav-Polishchuk <hello@karabeen.com>
2018-10-18 02:56:28 +0300

    update welcome text, add audio

--- a/js/main.js
+++ a/js/main.js
@@ -1,29 +1,103 @@
-$(".link-nav").on(
+$(".link-nav, .link-contacts").on(
   {
-    mouseenter: function () {
+    mouseenter: function() {
       $(this).addClass("is-hovered");
-      $(".link-nav").addClass
     },
-    mouseleave: function () {
+    mouseleave: function() {
       $(this).removeClass("is-hovered");
     }
   }
 );
 
-$(".link-contacts").on(
+$(".toggleContacts").click(function()
   {
-    mouseenter: function () {
-      $(this).addClass("is-hovered");
-      $(".link-contacts").addClass
-    },
-    mouseleave: function () {
-      $(this).removeClass("is-hovered");
-    }
+    $(".header").toggleClass("contacts-shown");
+    $(".shading-container").toggleClass("shown");
   }
 );
 
-function toggleContacts()
+var audioplayer = document.getElementById("name-pronunciation");
+
+var delay = (function()
 {
-  $(".header").toggleClass("contacts-shown");
-  $(".shading-container").toggleClass("shown");
-}
+  var timer = 0;
+  return function(callback, ms)
+  {
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+const points =
+{
+  left:
+    {
+      from: '15,0 50,25 50,75 15,100',
+      to: '15,10 40,10 40,90, 15,90'
+    },
+  right:
+    {
+      from: '50,25 85,50 85,50, 50,75',
+      to: '60,10 85,10 85,90, 60,90'
+    }
+};
+
+$(".btn-play").click(function()
+  {
+    if (audioplayer.paused)
+    {
+
+      $('#left').attr(
+        {
+          from: points.left.from ,
+          to: points.left.to
+        }
+      ).get(0).beginElement();
+
+      $('#right').attr(
+        {
+          from: points.right.from,
+          to: points.right.to
+        }
+      ).get(0).beginElement();
+
+      audioplayer.play();
+
+      delay(function()
+      {
+        $('#left').attr(
+          {
+            from: points.left.to ,
+            to: points.left.from
+          }
+        ).get(0).beginElement();
+
+        $('#right').attr(
+          {
+            from: points.right.to,
+            to: points.right.from
+          }
+        ).get(0).beginElement();
+      }, 1700 );
+    }
+
+    else
+    {
+      audioplayer.pause();
+
+      $('#left').attr(
+        {
+          from: points.left.to ,
+          to: points.left.from
+        }
+      ).get(0).beginElement();
+
+      $('#right').attr(
+        {
+          from: points.right.to,
+          to: points.right.from
+        }
+      ).get(0).beginElement();
+    }
+  }
+);
